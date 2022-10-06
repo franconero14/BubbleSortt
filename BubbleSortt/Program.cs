5b18a3978e48e515fe58c5176e66871336939da1
franconero14 <nerobalor@gmail.com>
2022-10-06 20:18:47 +0700

    class program

--- BubbleSortt/Program.cs
+++ BubbleSortt/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        // Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        // Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+    
