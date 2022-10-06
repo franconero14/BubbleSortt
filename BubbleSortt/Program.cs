790bf06746ae150799ee4664f21664343d89bfc1
franconero14 <nerobalor@gmail.com>
2022-10-06 20:26:07 +0700

    Method read

--- BubbleSortt/Program.cs
+++ BubbleSortt/Program.cs
@@ -9,4 +9,31 @@ namespace Bubble_Sort
 
         // Deklarasi variabel int untuk menyimpan banyaknya data pada array
         private int n;
-    
+
+        // Fungsi / Method untuk menerima masukan
+        public void read()
+        {
+            // Menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("_______________________");
+            Console.WriteLine(" Masukkan elemen array ");
+            Console.WriteLine("_______________________");
+
+            // Pengguna memasukkan elemen pada array
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
